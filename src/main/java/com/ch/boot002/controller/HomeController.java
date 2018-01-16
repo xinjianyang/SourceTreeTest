a1268b385a0747fb9ce1dc47b50d8cdc77d986ea
xinjian <xinjian6379@163.com>
2018-01-16 15:23:03 +0800

    第一次提交,熟悉git使用流程

---
+++ src/main/java/com/ch/boot002/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.ch.boot002.controller;
+
+
+import com.ch.boot002.util.ResultJson;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public ResultJson hello(){
+        return new ResultJson("jack","beijing",25);
+    }
+
+
+    @GetMapping("/home")
+    public ModelAndView helloJsp(){
+        ResultJson resultJson = new ResultJson("tomcat","beijing",45);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("res",resultJson);
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @GetMapping("/in")
+    public String getHello(Model model){
+        model.addAttribute("res","success");
+        return "index";
+    }
+}
---
+++ src/main/java/com/ch/boot002/util/ResultJson.java
@@ -0,0 +1,48 @@
+package com.ch.boot002.util;
+
+public class ResultJson {
+
+    private String name;
+    private String address;
+    private Integer age;
+
+
+
+
+
+
+    public ResultJson() {
+    }
+
+
+
+    public ResultJson(String name, String address, Integer age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
